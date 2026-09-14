6d1e3da5bbd61e58bb4ff8a2fc3e83fbd47d486a
Никита Манжиков <nikita.manzhikov@yandex.ru>
2026-09-14 22:41:45 +0300

    init

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -2,19 +2,40 @@ package ru.yandex.practicum.gym;
 
 import java.util.*;
 
+
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private final Map<DayOfWeek, Map<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        timetable.computeIfAbsent(trainingSession.getDayOfWeek(), k -> new HashMap<>())
+                .computeIfAbsent(trainingSession.getTimeOfDay(), k -> new ArrayList<>())
+                .add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public Map<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.getOrDefault(dayOfWeek, Collections.emptyMap());
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        Map<TimeOfDay, List<TrainingSession>> dayMap = timetable.get(dayOfWeek);
+        if (dayMap == null) {
+            return Collections.emptyList();
+        }
+        return dayMap.getOrDefault(timeOfDay, Collections.emptyList());
+    }
+
+    public List<Integer> getCountByCoaches() {
+        Map<String, Integer> coaches = new HashMap<>();
+        List<TrainingSession> temp = timetable.values().stream()
+                .flatMap(e -> e.values().stream())
+                .flatMap(List::stream)
+                .toList();
+        for (TrainingSession trainingSession : temp) {
+            String name = trainingSession.getCoach().getName() + trainingSession.getCoach().getMiddleName() + trainingSession.getCoach().getSurname();
+            coaches.put(name, coaches.computeIfAbsent(name, e -> 1) + 1);
+        }
+        return coaches.values().stream().sorted(Comparator.reverseOrder()).toList();
     }
 }
+
